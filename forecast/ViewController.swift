a3f77dd02c448467200a4f07bec0e117c97e4d51
taelee <ritmakid@gmail.com>
2021-05-10 23:12:12 +0900

    Initial Commit

---
+++ forecast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  forecast
+//
+//  Created by Terry Lee on 2021/05/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
