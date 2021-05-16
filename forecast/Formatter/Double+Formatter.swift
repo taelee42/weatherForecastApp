fbe0ce5de999154db133a8f89221541fefa9092b
taelee <ritmakid@gmail.com>
2021-05-16 11:54:12 +0900

    Add temperature formatter

---
+++ forecast/Formatter/Double+Formatter.swift
@@ -0,0 +1,23 @@
+//
+//  Double+Formatter.swift
+//  forecast
+//
+//  Created by Terry Lee on 2021/05/15.
+//
+
+import Foundation
+
+fileprivate let temperatureFormatter: MeasurementFormatter = {
+    let f = MeasurementFormatter()
+    f.locale = Locale(identifier: "ko_kr")
+    f.numberFormatter.maximumFractionDigits = 1
+    f.unitOptions = .temperatureWithoutUnit
+    return f
+}()
+
+extension Double {
+    var temperatureString: String {
+        let temp = Measurement<UnitTemperature>(value: self, unit: .celsius)
+        return temperatureFormatter.string(from: temp)
+    }
+}
--- forecast/ViewController.swift
+++ forecast/ViewController.swift
@@ -6,13 +6,18 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var listTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let location = CLLocation(latitude: 37.498206, longitude: 127.02761)
+        WeatherDataSource.shared.fetch(location: location) {
+            self.listTableView.reloadData()
+        }
     }
 
 
@@ -33,6 +38,13 @@ extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.section == 0 {
             let cell = tableView.dequeueReusableCell(withIdentifier: "SummaryTableViewCell") as! SummaryTableViewCell
+            
+            if let weather = WeatherDataSource.shared.summary?.weather.first, let main = WeatherDataSource.shared.summary?.main {
+                cell.weatherImageView.image = UIImage(named: weather.icon)
+                cell.statusLabel.text = weather.description
+                cell.minMaxLabel.text = "최고 \(main.temp_max.temperatureString) 최소 \(main.temp_min.temperatureString)"
+                cell.currentTemperatureLabel.text = "\(main.temp.temperatureString)"
+            }
             return cell
         }
         let cell = tableView.dequeueReusableCell(withIdentifier: "ForecastTableViewCell") as! ForecastTableViewCell
