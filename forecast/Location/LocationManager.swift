ba12636fc268bb4e3e87a9a10e5fdf30dd2e6e7f
taelee <ritmakid@gmail.com>
2021-05-17 14:09:07 +0900

    Add location manager

---
+++ forecast/Location/LocationManager.swift
@@ -0,0 +1,96 @@
+//
+//  LocationManager.swift
+//  forecast
+//
+//  Created by Terry Lee on 2021/05/17.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject {
+    static let shared = LocationManager()
+    private override init() {
+        manager = CLLocationManager()
+        manager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+        
+        super.init()
+        
+        manager.delegate = self
+    }
+    
+    let manager: CLLocationManager
+    
+    func updateLocation() {
+        let status: CLAuthorizationStatus
+        
+        if #available(iOS 14.0, *) {
+            status = manager.authorizationStatus
+        } else {
+            status = CLLocationManager.authorizationStatus()
+        }
+        
+        switch status {
+        case .notDetermined:
+            requestAuthorization()
+        case .authorizedAlways, .authorizedWhenInUse:
+            requestCurrentLocation()
+        case .denied, .restricted:
+            print("not available")
+        default:
+            print("unknown")
+        }
+    }
+}
+
+
+extension LocationManager: CLLocationManagerDelegate {
+    //권한을 요청하는 코드
+    //이 메소드가 아래 requestCurrentLocation보다 먼저 호출돼야 한다.
+    private func requestAuthorization() {
+        manager.requestWhenInUseAuthorization()
+    }
+    //현재 위치를 요청하는 코드
+    private func requestCurrentLocation() {
+//        manager.startUpdatingLocation()
+        manager.requestLocation()
+        //이 메소드가 제대로 실행되면 아래 didUpdateLocations가 호출되고
+        //실패하면 didFailWithError 메소드가 호출된다.
+    }
+    
+    @available(iOS 14.0, *)
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .authorizedAlways, .authorizedWhenInUse:
+            requestCurrentLocation()
+        case .notDetermined, .denied, .restricted:
+            print("not available")
+        default:
+            print("unknown")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse:
+            requestCurrentLocation()
+        case .notDetermined, .denied, .restricted:
+            print("not available")
+        default:
+            print("unknown")
+        }
+    }
+    
+    //새로운 위치정보가 전달되면 반복적으로 호출됨
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("hi")
+        print(locations.last)
+    }
+    
+    //에러가 발생되면 호출되는 메소드
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
+    
+}
